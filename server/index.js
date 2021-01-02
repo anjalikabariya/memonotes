5c3af492fd8d977edd2e2ac43ddefa9819fde98d
anjalikabariya <anjali.kabariya21@gmail.com>
2021-01-02 18:31:32 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -7,17 +7,19 @@ import cors from 'cors';
 import noteRoutes from './routes/notes.js';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/notes', noteRoutes);
+app.get('/', (req, res) => {
+  res.send('hello to first app');
+});
 
-const CONNECTION_URL = 'mongodb+srv://appuser:appuser@cluster0.ehsl0.mongodb.net/Memonotes?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
